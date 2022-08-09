795bf047ed0009d1ec092adaa166255880495a41
GilsonRengel <hiperbyte@globo.com>
2022-08-09 18:00:02 -0300

    Aula 04 de prog imperativa

---
+++ Aula04/index.js
@@ -0,0 +1,10 @@
+// Uso do Console.log, que é uma ferramenta de desenvolvedor
+
+console.log("Olá Mundo!");
+
+// Vimos os tipos de dados (number, string, boolean) e operadores matemáticos e lógicos.
+
+// Variáveis -> var, let, const
+// Var -> aplicada no escopo global
+// Let -> é reconhecida somente dentro do bloco
+// Const -> valor atribuído não pode ser alterado
